5c22fdfd6695f93ff9fafd9d36a3f5fb97d3849f
Muhammad Fauzi Dwikurnia <muhfauzidk@gmail.com>
2022-03-25 16:19:07 +0700

    menambahkan class SayaTubeVideo

--- tpmodul5_1302194027/Program.cs
+++ tpmodul5_1302194027/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        //Generate 5 digit random number
+        Random r = new Random();
+        this.id = r.Next(100000);
+        string fiveDigit = this.id.ToString("D5");
+
+        playCount = 0;
+        this.title = title;
+    }
+
+    public void IncreasePlayCount(int jmlAngka)
+    {
+        this.playCount = jmlAngka;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("id: " + this.id);
+        Console.WriteLine("title: " + this.title);
+        Console.WriteLine("play count: " + this.playCount);
+        
+    }
+    public static void Main()
+    {
+        string judul = "Tutorial Design By Contract - Muhammad Fauzi Dwikurnia";
+        //Create object of class SayaTubeVideo
+        SayaTubeVideo p = new SayaTubeVideo(judul);
+
+        p.IncreasePlayCount(1);
+        p.PrintVideoDetails();
+    }
+}
